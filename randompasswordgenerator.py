2b6c96fc7ad264aab74330e4a15593d4c773314e
ayyappakamuju007 <ayyappakamuju@gmail.com>
2024-06-20 21:15:24 +0530

    Add files via upload

---
+++ randompasswordgenerator.py
@@ -0,0 +1,7 @@
+
+import string
+import random
+all_characters = string.ascii_letters + string.digits + string.punctuation
+length = int(input("Enter the length of the password: "))
+password = ''.join(random.choices(all_characters, k=length))4
+print("Your password is:", password)
